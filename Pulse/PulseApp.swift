caa84f013166cfc6ea53ec6a1a79214a837398e0
lli205 <lli205@illinois.edu>
2024-03-04 16:58:58 -0600

    Initial Commit

---
+++ Pulse/PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseApp.swift
+//  Pulse
+//
+//  Created by Litao Li on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
