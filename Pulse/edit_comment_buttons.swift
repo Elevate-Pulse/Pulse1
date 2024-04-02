6566781c157be48ee4c9d1a20bacc4e7ad4150ba
yjian29 <120494191+yjian29@users.noreply.github.com>
2024-04-02 13:55:41 -0500

    bar graph spacing / some firebase functions

---
+++ Pulse/edit_comment_buttons.swift
@@ -0,0 +1,18 @@
+//
+//  edit_comment_buttons.swift
+//  Pulse
+//
+//  Created by Yinglin Jiang on 3/20/24.
+//
+
+import SwiftUI
+
+struct edit_comment_buttons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    edit_comment_buttons()
+}
