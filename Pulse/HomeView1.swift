22b1cf8118e5766201b518ce4b024f709197434c
pguan4 <sharkerwill23@gmail.com>
2024-04-07 14:58:16 -0500

    redid the app

---
+++ Pulse/HomeView1.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView1.swift
+//  Pulse
+//
+//  Created by Peter Guan on 4/5/24.
+//
+
+import SwiftUI
+
+struct HomeView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView1()
+}
