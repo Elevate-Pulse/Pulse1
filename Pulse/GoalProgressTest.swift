5b662c4c4fa64720bd1a128fd1eea812dbbfd796
lli205 <lli205@illinois.edu>
2024-04-09 19:05:57 -0500

    goal progress and friend

---
+++ Pulse/GoalProgressTest.swift
@@ -0,0 +1,18 @@
+//
+//  GoalProgressTest.swift
+//  Pulse
+//
+//  Created by Litao Li on 4/9/24.
+//
+
+import SwiftUI
+
+struct GoalProgressTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoalProgressTest()
+}
